c606e2e58e2097e249ec3b0b7e1346e1bb85cf51
keshav1017 <prasadkeshav003@gmail.com>
2025-06-10 18:21:45 +0530

    Experiment notebook added

--- demo.py
+++ demo.py
@@ -9,13 +9,13 @@
 
 # -------------------------------------------------------------------------------------
 
-# below is the code to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# # below is the code to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1 + 'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1 + 'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
